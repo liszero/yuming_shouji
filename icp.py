3d5ff1dc7dbb8c3dbaa9d9bd533428226f040ddc
lisa-hue <78603109+lisa-hue@users.noreply.github.com>
2021-12-19 16:17:01 +0800

    Update icp.py

--- icp.py
+++ icp.py
@@ -73,45 +73,48 @@ def chinazApi(domain):
     except Exception as e:
         print('[error] request : {}\n{}'.format(url, e.args))
         return []
+    try:
+        json_ret = json.loads(res.text)
+        # print(json_ret)
+        if 'amount' not in json_ret.keys():
+            return chinazNewDomains
+        amount = json_ret['amount']
+        pages = math.ceil(amount / 20)
+        print('页数: {}'.format(pages))
+        # 解析返回的json数据包，过滤出公司名，域名，时间
+        tempList.extend(parse_json(json_ret))
+        # for _ in chinazNewDomains:
+        #     print(_)
 
-    json_ret = json.loads(res.text)
-    # print(json_ret)
-    if 'amount' not in json_ret.keys():
-        return chinazNewDomains
-    amount = json_ret['amount']
-    pages = math.ceil(amount / 20)
-    print('页数: {}'.format(pages))
-    # 解析返回的json数据包，过滤出公司名，域名，时间
-    tempList.extend(parse_json(json_ret))
-    # for _ in chinazNewDomains:
-    #     print(_)
-
-    # 继续获取后面页数
-    for page in range(2, pages+1):
-        print('请求第{}页'.format(page))
-        data = 'pageNo={}&pageSize=20&Kw='.format(page) + companyNameUrlEncode
-        try:
-            res = requests.post(url=url, headers=headers, data=data, allow_redirects=False, verify=False)
-            json_ret = json.loads(res.text)
-            tempList.extend(parse_json(json_ret))
-        except Exception as e:
-            print('[error] request : {}\n{}'.format(url, e.args))
+        # 继续获取后面页数
+        for page in range(2, pages+1):
+            print('请求第{}页'.format(page))
+            data = 'pageNo={}&pageSize=20&Kw='.format(page) + companyNameUrlEncode
+            try:
+                res = requests.post(url=url, headers=headers, data=data, allow_redirects=False, verify=False)
+                json_ret = json.loads(res.text)
+                tempList.extend(parse_json(json_ret))
+            except Exception as e:
+                print('[error] request : {}\n{}'.format(url, e.args))
 
 
-    for each in tempList:
-        if each[1] not in tempDict:
-            tempDict[each[1]] = each
-            chinazNewDomains.append(each)
+        for each in tempList:
+            if each[1] not in tempDict:
+                tempDict[each[1]] = each
+                chinazNewDomains.append(each)
 
-    print('chinazApi去重后共计{}个顶级域名'.format(len(chinazNewDomains)))
-    # for _ in chinazNewDomains:
-    #     print(_)
-    if len(chinazNewDomains) > 0:
-        with open('result.txt','a+',encoding="utf-8",errors="ignore") as w:
-            for newdomain in chinazNewDomains:
-                if check_contain_chinese(newdomain[1]):
-                    continue
-                w.write(newdomain[1]+"\n")
+        print('chinazApi去重后共计{}个顶级域名'.format(len(chinazNewDomains)))
+        # for _ in chinazNewDomains:
+        #     print(_)
+        if len(chinazNewDomains) > 0:
+            with open('laravel_result.txt','a+',encoding="utf-8",errors="ignore") as w:
+                for newdomain in chinazNewDomains:
+                    if check_contain_chinese(newdomain[1]):
+                        continue
+                    w.write(newdomain[1]+"\n")
+    except Exception as e:
+        print(e)
+    
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
